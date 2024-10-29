175b9dd8015fc1f92ed3b303a777da3f4dcd62b5
Daria Metelitsa <dariametelitsa@gmail.com>
2024-10-29 18:30:40 +0300

    add button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react'
+import { ComponentPropsWithoutRef } from 'react'
+
+import { Slot } from '@radix-ui/react-slot'
+import { VariantProps, cva } from 'class-variance-authority'
+import { cn } from "../../utils/cn.ts";
+
+export const buttonVariants = cva(
+  ['rounded-sm px-6 py-1.5 inline-flex justify-center text-white'],
+  {
+    defaultVariants: {
+      variant: 'primary',
+    },
+    variants: {
+      variant: {
+        outline: [
+          'text-accent-500',
+          'border',
+          'border-solid',
+          'border-accent-500',
+          'hover:text-accent-100 hover:border-accent-100',
+          'focus-within:border-accent-700',
+          'focus-within:outline focus-within:outline-2',
+          'focus-within:outline-accent-700',
+          'focus-within:text-accent-700',
+          'disabled:border-accent-900 disabled:text-accent-900',
+        ],
+        primary: [
+          'bg-accent-500',
+          'active:bg-accent-700',
+          'hover:bg-accent-100',
+          'focus-within:outline focus-within:outline-2 focus-within:outline-accent-700',
+          'disabled:bg-accent-900 disabled:text-light-900',
+        ],
+        secondary: [
+          'bg-dark-300',
+          'active:bg-dark-400',
+          'hover:bg-dark-100',
+          'focus-within:outline focus-within:outline-1 focus-within:outline-accent-300',
+          'disabled:bg-dark-500 disabled:text-light-900',
+        ],
+        text: [
+          'text-accent-500',
+          'active:text-accent-700',
+          'hover:text-accent-100',
+          'focus-within:outline focus-within:outline-2',
+          'focus-within:outline-accent-700',
+          'focus-within:text-accent-700',
+          'disabled:text-accent-900',
+        ],
+      },
+    },
+  }
+)
+
+type ButtonProps = { asChild?: boolean } & ComponentPropsWithoutRef<'button'> &
+  VariantProps<typeof buttonVariants>
+
+const Button = React.forwardRef<React.ElementRef<'button'>, ButtonProps>(
+  ({ asChild, className, variant = 'primary', ...props }, ref) => {
+    const Component = asChild ? Slot : 'button'
+
+    return (
+      <Component
+        className={cn(buttonVariants({ variant }), className)}
+        {...ref}
+        {...props}
+      />
+    )
+  }
+)
+
+Button.displayName = 'Button'
+
+export { Button }
