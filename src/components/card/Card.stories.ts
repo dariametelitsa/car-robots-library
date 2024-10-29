c055f0b82f32e735a30a12342c702d0c02fcb0e5
Daria Metelitsa <dariametelitsa@gmail.com>
2024-10-29 18:12:25 +0300

    add component

---
+++ src/components/card/Card.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Card } from "./Card.tsx";
+
+const meta = {
+  component: Card,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  title: 'Component/Card',
+} satisfies Meta<typeof Card>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const BasicCard: Story = {
+  args: {
+    children: 'Basic Card',
+  },
+}
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from 'react'
+
+import { Slot } from '@radix-ui/react-slot'
+import { cn } from "../../utils/cn.ts";
+
+type Props = { asChild?: boolean } & ComponentProps<'div'>
+
+export const Card = ({ asChild, children, className, ...props }: Props) => {
+  const Component = asChild ? Slot : 'div'
+
+  return (
+    <Component
+      className={cn(
+        `bg-dark-500 rounded-sm border border-dark-300 px-6 py-4 text-light-100`,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  )
+}
