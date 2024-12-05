278549726151e0c98cedabb34c03ab589e092e2f
dariametelitsa <109726679+dariametelitsa@users.noreply.github.com>
2024-12-05 13:37:25 +0300

    Fix slider (#9)
    * feat: fix slider story
    
    * feat: delete ref from linkWith Icon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import "./index.css"
+import "./index.css";
 
-export * from './components'
-export * from './assets/icons/filledIcons'
-export * from './assets/icons/outlineIcons'
+export * from "./components";
+export * from "./assets/icons/filledIcons";
+export * from "./assets/icons/outlineIcons";
