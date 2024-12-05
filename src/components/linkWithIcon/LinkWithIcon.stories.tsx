28ca71ee62793e14e624cb3d9d8032f8b47e792e
dariametelitsa <109726679+dariametelitsa@users.noreply.github.com>
2024-12-05 11:49:17 +0300

    fix: fix component link (#6)

--- src/components/linkWithIcon/LinkWithIcon.stories.tsx
+++ src/components/linkWithIcon/LinkWithIcon.stories.tsx
@@ -1,46 +1,38 @@
-import { Desktop } from '@/assets/icons/filledIcons'
-import Chrome from '@/assets/icons/filledIcons/Chrome'
-import { LinkWithIcon } from '@/common'
-import { Meta, StoryObj } from '@storybook/react'
-import Link from 'next/link'
+import { Meta, StoryObj } from "@storybook/react";
+import { LinkWithIcon } from "./LinkWithIcon.tsx";
+import { Chrome, Desktop } from "../../assets/icons";
 
 const meta = {
   args: {
     ActiveIcon: Chrome,
     DefaultIcon: Chrome,
-    as: Link,
-    children: 'Chrome',
+    as: "a",
+    children: "Chrome",
     disabled: false,
-    href: '/123',
+    href: "/123",
   },
   component: LinkWithIcon,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-  title: 'Component/LinkWithIcon',
-} satisfies Meta<typeof LinkWithIcon>
+  tags: ["autodocs"],
+  title: "Component/LinkWithIcon",
+} satisfies Meta<typeof LinkWithIcon>;
 
-export default meta
+export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
 export const LinkWithIconDefault: Story = {
-  args: {
-    ActiveIcon: Chrome,
-    DefaultIcon: Chrome,
-    as: Link,
-    children: 'Chrome',
-    disabled: false,
-    href: '/123',
-  },
-}
+  args: {},
+};
 
 export const ButtonWithIconDefault: Story = {
   args: {
     ActiveIcon: Chrome,
     DefaultIcon: Desktop,
-    as: 'button',
-    children: 'Chrome',
+    as: "button",
+    children: "Desktop",
     disabled: false,
   },
-}
+};
