ed2affa9dc445e0e3fa9b44f7852211882a1d1bb
Daria Metelitsa <dariametelitsa@gmail.com>
2024-12-05 11:48:26 +0300

    fix: fix component link

--- src/components/linkWithIcon/LinkWithIcon.tsx
+++ src/components/linkWithIcon/LinkWithIcon.tsx
@@ -1,22 +1,19 @@
-import React, { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
-
-import { cn } from '@/common/utils/cn'
-import { getBasePath } from '@/common/utils/getBasePath'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React, { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+import { cn } from "../../utils/cn.ts";
 
 const styles = {
-  activeLink: 'text-accent-500 font-bold',
-  link: 'flex items-center gap-3 [&_svg]:w-6 [&_svg]:h-6 rounded-sm focus-visible:border-transparent focus-visible:ring-2 focus-visible:ring-accent-700 focus-visible:outline-none hover:text-accent-100',
-}
+  activeLink: "text-accent-500 font-bold",
+  link: "flex items-center gap-3 [&_svg]:w-6 [&_svg]:h-6 rounded-sm focus-visible:border-transparent focus-visible:ring-2 focus-visible:ring-accent-700 focus-visible:outline-none hover:text-accent-100",
+};
 
-type Props<T extends ElementType = typeof Link> = {
-  ActiveIcon?: ReactNode
-  DefaultIcon: ReactNode
-  as?: T
-  disabled?: boolean
-  iconTrigger?: boolean | string
-} & ComponentPropsWithoutRef<T>
+type Props<T extends ElementType> = {
+  ActiveIcon?: ReactNode;
+  DefaultIcon: ReactNode;
+  as?: T;
+  disabled?: boolean;
+  iconTrigger?: boolean | string;
+  isActive?: boolean;
+} & ComponentPropsWithoutRef<T>;
 
 export const LinkWithIcon = React.forwardRef<HTMLElement, Props<ElementType>>(
   function NavLink(
@@ -28,21 +25,12 @@ export const LinkWithIcon = React.forwardRef<HTMLElement, Props<ElementType>>(
       className,
       disabled,
       href,
-      iconTrigger,
+      isActive = false,
       ...rest
     },
-    ref
+    ref,
   ) {
-    const Component = as || Link
-    const { pathname } = useRouter()
-
-    let isActive: boolean
-
-    if (Component === Link) {
-      isActive = getBasePath(pathname) === getBasePath(href)
-    } else {
-      isActive = !!iconTrigger
-    }
+    const Component = as || "a";
 
     return (
       <Component
@@ -53,7 +41,7 @@ export const LinkWithIcon = React.forwardRef<HTMLElement, Props<ElementType>>(
           className,
           styles.link,
           isActive && styles.activeLink,
-          disabled && 'pointer-events-none text-dark-100'
+          disabled && "pointer-events-none text-dark-100",
         )}
         ref={ref}
         tabIndex={disabled ? -1 : undefined}
@@ -61,8 +49,8 @@ export const LinkWithIcon = React.forwardRef<HTMLElement, Props<ElementType>>(
         {isActive && ActiveIcon ? <ActiveIcon /> : <DefaultIcon />}
         {children}
       </Component>
-    )
-  }
-)
+    );
+  },
+);
 
-LinkWithIcon.displayName = 'LinkWithIcon'
+LinkWithIcon.displayName = "LinkWithIcon";
