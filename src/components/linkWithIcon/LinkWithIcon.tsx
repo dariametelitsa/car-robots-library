f670efa47b5ad503b06ba0ec56469b366a8b5bf5
Daria Metelitsa <dariametelitsa@gmail.com>
2024-12-05 13:36:42 +0300

    feat: delete ref from linkWith Icon

--- src/components/linkWithIcon/LinkWithIcon.tsx
+++ src/components/linkWithIcon/LinkWithIcon.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+import { ComponentPropsWithoutRef, ElementType } from "react";
 import { cn } from "../../utils/cn.ts";
 
 const styles = {
@@ -6,51 +6,43 @@ const styles = {
   link: "flex items-center gap-3 [&_svg]:w-6 [&_svg]:h-6 rounded-sm focus-visible:border-transparent focus-visible:ring-2 focus-visible:ring-accent-700 focus-visible:outline-none hover:text-accent-100",
 };
 
-type Props<T extends ElementType> = {
-  ActiveIcon?: ReactNode;
-  DefaultIcon: ReactNode;
+type Props<T extends ElementType = "a"> = {
+  ActiveIcon?: ElementType;
+  DefaultIcon: ElementType;
   as?: T;
   disabled?: boolean;
   iconTrigger?: boolean | string;
   isActive?: boolean;
 } & ComponentPropsWithoutRef<T>;
 
-export const LinkWithIcon = React.forwardRef<HTMLElement, Props<ElementType>>(
-  (
-    {
-      ActiveIcon,
-      DefaultIcon,
-      as,
-      children,
-      className,
-      disabled,
-      href,
-      isActive = false,
-      ...rest
-    },
-    ref,
-  ) => {
-    const Component = as || "a";
+export const LinkWithIcon = <T extends ElementType = "a">({
+  ActiveIcon,
+  DefaultIcon,
+  as,
+  children,
+  className,
+  disabled,
+  href,
+  isActive = false,
+  ...rest
+}: Props<T>) => {
+  const Component = as || "a";
 
-    return (
-      <Component
-        href={href}
-        {...rest}
-        aria-disabled={disabled}
-        className={cn(
-          className,
-          styles.link,
-          isActive && styles.activeLink,
-          disabled && "pointer-events-none text-dark-100",
-        )}
-        ref={ref}
-        tabIndex={disabled ? -1 : undefined}
-      >
-        {isActive && ActiveIcon ? <ActiveIcon /> : <DefaultIcon />}
-        {children}
-      </Component>
-    );
-  },
-);
-
-LinkWithIcon.displayName = "LinkWithIcon";
+  return (
+    <Component
+      href={href}
+      {...rest}
+      aria-disabled={disabled}
+      className={cn(
+        className,
+        styles.link,
+        isActive && styles.activeLink,
+        disabled && "pointer-events-none text-dark-100",
+      )}
+      tabIndex={disabled ? -1 : undefined}
+    >
+      {isActive && ActiveIcon ? <ActiveIcon /> : <DefaultIcon />}
+      {children}
+    </Component>
+  );
+};
