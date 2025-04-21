5b2d22d3afe7f42a3c936815bda796d2ce37cda7
dariametelitsa <dariametelitsa@gmail.com>
2025-04-21 11:07:36 +0300

    add screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,15 @@ export default {
       spacing: {
         scrollbar: "16px",
       },
+      screens: {
+        'sm-500': '500px',
+        xs: '360px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      },
     },
   },
   variants: {
